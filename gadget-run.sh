73281675878728a7609cb5e436cc0daed6e45fb1
peterbay <pb.pb@centrum.cz>
2021-08-22 15:48:04 +0200

    Fixed creation of configuration in gadget-run.sh when the configuration is already defined and active.

--- gadget-run.sh
+++ gadget-run.sh
@@ -21,6 +21,17 @@ GADGET_PATH="${CONFIGFS_PATH}/usb_gadget/${GADGET_NAME}"
 
 echo "INFO:  Gadget config path: ${GADGET_PATH}"
 
+UDC_INTERFACE=$(ls /sys/class/udc)
+if [ -z "${UDC_INTERFACE}" ]; then
+    echo "ERROR: UDC interface: NOT FOUND"
+else
+    echo "INFO:  UDC interface: ${UDC_INTERFACE}"
+fi
+
+if [ -e "/sys/class/udc/$UDC_INTERFACE/uevent" ]; then
+    . "/sys/class/udc/$UDC_INTERFACE/uevent"
+fi
+
 # Include configuration files
 for CONFIG_PATH in "$@"
 do
@@ -65,101 +76,105 @@ EOF
 
 # Gadget configfs initialization
 
-mkdir $GADGET_PATH
+if [ -z "${USB_UDC_DRIVER}" ]; then
 
-echo 0x1d6b > $GADGET_PATH/idVendor
-echo 0x0104 > $GADGET_PATH/idProduct
-echo 0x0100 > $GADGET_PATH/bcdDevice
-echo 0x0200 > $GADGET_PATH/bcdUSB
+    mkdir -p $GADGET_PATH
 
-echo 0xEF   > $GADGET_PATH/bDeviceClass
-echo 0x02   > $GADGET_PATH/bDeviceSubClass
-echo 0x01   > $GADGET_PATH/bDeviceProtocol
+    echo 0x1d6b > $GADGET_PATH/idVendor
+    echo 0x0104 > $GADGET_PATH/idProduct
+    echo 0x0100 > $GADGET_PATH/bcdDevice
+    echo 0x0200 > $GADGET_PATH/bcdUSB
 
-mkdir $GADGET_PATH/strings/0x409
-echo "${SERIAL_NUMBER}" > $GADGET_PATH/strings/0x409/serialnumber
-echo "${MANUFACTURER}"  > $GADGET_PATH/strings/0x409/manufacturer
-echo "${PRODUCT_NAME}"  > $GADGET_PATH/strings/0x409/product
+    echo 0xEF   > $GADGET_PATH/bDeviceClass
+    echo 0x02   > $GADGET_PATH/bDeviceSubClass
+    echo 0x01   > $GADGET_PATH/bDeviceProtocol
 
-mkdir $GADGET_PATH/configs/c.2
-mkdir $GADGET_PATH/configs/c.2/strings/0x409
-echo 500 > $GADGET_PATH/configs/c.2/MaxPower
+    mkdir -p $GADGET_PATH/strings/0x409
+    echo "${SERIAL_NUMBER}" > $GADGET_PATH/strings/0x409/serialnumber
+    echo "${MANUFACTURER}"  > $GADGET_PATH/strings/0x409/manufacturer
+    echo "${PRODUCT_NAME}"  > $GADGET_PATH/strings/0x409/product
 
-if [ "${INIT_UVC}" = true ]; then
+    mkdir -p $GADGET_PATH/configs/c.2
+    mkdir -p $GADGET_PATH/configs/c.2/strings/0x409
+    echo 500 > $GADGET_PATH/configs/c.2/MaxPower
 
-    echo "INFO:  UVC init"
+    if [ "${INIT_UVC}" = true ]; then
 
-    MJPEG=false
-    UNCOMPRESSED=false
+        echo "INFO:  UVC init"
 
-    UVC_FUNCTION_PATH="${GADGET_PATH}/functions/uvc.usb0"
+        MJPEG=false
+        UNCOMPRESSED=false
 
-    mkdir "${UVC_FUNCTION_PATH}"
+        UVC_FUNCTION_PATH="${GADGET_PATH}/functions/uvc.usb0"
 
-    mkdir -p "${UVC_FUNCTION_PATH}/control/header/h"
-    ln -s "${UVC_FUNCTION_PATH}/control/header/h" "${UVC_FUNCTION_PATH}/control/class/fs/h"
+        mkdir -p "${UVC_FUNCTION_PATH}"
 
-    if [ -z "${UVC_FORMATS}" ]; then
-        echo "ERROR: Missing UVC_FORMATS"
-        exit 2
-    fi
+        mkdir -p "${UVC_FUNCTION_PATH}/control/header/h"
+        ln -s "${UVC_FUNCTION_PATH}/control/header/h" "${UVC_FUNCTION_PATH}/control/class/fs/h"
 
-    if [ ! -z "${STREAMING_MAXPACKET}" ]; then
-        echo "${STREAMING_MAXPACKET}" > "${UVC_FUNCTION_PATH}/streaming_maxpacket"
-    fi
-
-
-    for FORMAT in "${UVC_FORMATS[@]}"; do
-        FORMAT_ARRAY=($(echo "${FORMAT}" | tr ':' '\n'))
-
-        echo "INFO:  Frame format - format: ${FORMAT_ARRAY[0]}, fps: ${FORMAT_ARRAY[2]}, width: ${FORMAT_ARRAY[3]}, height: ${FORMAT_ARRAY[4]}"
-
-        uvc_config_frame "${FORMAT_ARRAY[0]}" "${FORMAT_ARRAY[1]}" "${FORMAT_ARRAY[2]}" "${FORMAT_ARRAY[3]}" "${FORMAT_ARRAY[4]}"
-
-        if [ "${FORMAT_ARRAY[0]}" = "mjpeg" ]; then
-            MJPEG=true
+        if [ -z "${UVC_FORMATS}" ]; then
+            echo "ERROR: Missing UVC_FORMATS"
+            exit 2
         fi
 
-        if [ "${FORMAT_ARRAY[0]}" = "uncompressed" ]; then
-            UNCOMPRESSED=true
+        if [ ! -z "${STREAMING_MAXPACKET}" ]; then
+            echo "${STREAMING_MAXPACKET}" > "${UVC_FUNCTION_PATH}/streaming_maxpacket"
         fi
-    done
 
-    mkdir "${UVC_FUNCTION_PATH}/streaming/header/h"
-    cd "${UVC_FUNCTION_PATH}/streaming/header/h"
 
-    if [ "${MJPEG}" = true ]; then
-        ln -s "${UVC_FUNCTION_PATH}/streaming/mjpeg/m"
+        for FORMAT in "${UVC_FORMATS[@]}"; do
+            FORMAT_ARRAY=($(echo "${FORMAT}" | tr ':' '\n'))
+
+            echo "INFO:  Frame format - format: ${FORMAT_ARRAY[0]}, fps: ${FORMAT_ARRAY[2]}, width: ${FORMAT_ARRAY[3]}, height: ${FORMAT_ARRAY[4]}"
+
+            uvc_config_frame "${FORMAT_ARRAY[0]}" "${FORMAT_ARRAY[1]}" "${FORMAT_ARRAY[2]}" "${FORMAT_ARRAY[3]}" "${FORMAT_ARRAY[4]}"
+
+            if [ "${FORMAT_ARRAY[0]}" = "mjpeg" ]; then
+                MJPEG=true
+            fi
+
+            if [ "${FORMAT_ARRAY[0]}" = "uncompressed" ]; then
+                UNCOMPRESSED=true
+            fi
+        done
+
+        mkdir -p "${UVC_FUNCTION_PATH}/streaming/header/h"
+        cd "${UVC_FUNCTION_PATH}/streaming/header/h"
+
+        if [ "${MJPEG}" = true ]; then
+            ln -s "${UVC_FUNCTION_PATH}/streaming/mjpeg/m"
+        fi
+
+        if [ "${UNCOMPRESSED}" = true ]; then
+            ln -s "${UVC_FUNCTION_PATH}/streaming/uncompressed/u"
+        fi
+
+        cd "${UVC_FUNCTION_PATH}/streaming/class/fs"
+        ln -s ../../header/h
+
+        cd "${UVC_FUNCTION_PATH}/streaming/class/hs"
+        ln -s ../../header/h
+
+        cd "${UVC_FUNCTION_PATH}/streaming/class/ss"
+        ln -s ../../header/h
+
+        ln -s "${UVC_FUNCTION_PATH}" $GADGET_PATH/configs/c.2/uvc.usb0
+
     fi
 
-    if [ "${UNCOMPRESSED}" = true ]; then
-        ln -s "${UVC_FUNCTION_PATH}/streaming/uncompressed/u"
+    if [ "${INIT_SERIAL}" = true ]; then
+
+        echo "INFO:  SERIAL init"
+
+        mkdir -p $GADGET_PATH/functions/acm.usb0
+        ln -s $GADGET_PATH/functions/acm.usb0 $GADGET_PATH/configs/c.2/acm.usb0
+
     fi
 
-    cd "${UVC_FUNCTION_PATH}/streaming/class/fs"
-    ln -s ../../header/h
-
-    cd "${UVC_FUNCTION_PATH}/streaming/class/hs"
-    ln -s ../../header/h
-
-    cd "${UVC_FUNCTION_PATH}/streaming/class/ss"
-    ln -s ../../header/h
-
-    ln -s "${UVC_FUNCTION_PATH}" $GADGET_PATH/configs/c.2/uvc.usb0
+    udevadm settle -t 5 || :
+    ls /sys/class/udc > $GADGET_PATH/UDC
 
 fi
-
-if [ "${INIT_SERIAL}" = true ]; then
-
-    echo "INFO:  SERIAL init"
-
-    mkdir $GADGET_PATH/functions/acm.usb0
-    ln -s $GADGET_PATH/functions/acm.usb0 $GADGET_PATH/configs/c.2/acm.usb0
-
-fi
-
-udevadm settle -t 5 || :
-ls /sys/class/udc > $GADGET_PATH/UDC
 
 echo "INFO:  --- Gadget initialization finish ---"
 
